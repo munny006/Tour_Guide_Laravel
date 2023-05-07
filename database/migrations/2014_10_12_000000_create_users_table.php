8cee782b074d5cc84fc44d2aaccc6fdb9d49255d
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-05-08 02:39:15 +0600

    some pblm fixed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,12 +21,13 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image')->default('default.jpg');
+            $table->string('image');
+            // ->default('default.jpg');
             $table->text('about')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
-            
+
 
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
       data-image-src="{{asset('frontend/img/nilachal-bandarban-01.jpg')}}" style="height: 820px;">
       <div class="overlay-bg overlay"></div>
       <div class="container">
- 
+
         <div class="row fullscreen">
           <div class="banner-content d-flex align-items-center col-lg-12 col-md-12">
             <h1 style="float: left;margin-left: 144px;">
@@ -66,9 +66,9 @@
             @foreach($posts as $post)
         <div class="single-posts col-lg-4 col-sm-4 mb-3">
             <div class="item single-cat">
-            <div  style="width:190px">
+            {{-- <div  style="width:190px"> --}}
             <img src="{{asset('storage/post/'.$post->image)}}" class="img-fluid " alt="{{$post->image}}" style="width:1000px; height: 200px;"/>
-           </div>
+           {{-- </div> --}}
 
             <div>
             <p class="date" style="float: left;margin-left: -1px; width: 140px;">{{$post->created_at->diffForHumans()}}</p>
@@ -106,20 +106,37 @@
         <div class="row justify-content-center">
          @foreach($posts as $post)
           <div class="single-posts col-lg-4 col-sm-4 mb-3 py-3"style="width:250px">
-            <img class="img-fluid  mx-auto" src="{{asset('storage/post/'.$post->image)}}" alt="{{$post->image}}"style="width:1000px; height: 250px;">
+            <img class="img-fluid  mx-auto" src="{{asset('storage/post/'.$post->image)}}" alt="{{$post->image}}"style="width:1000px; height: 200px;">
             <div class="date mt-20 mb-20" style=" width: 140px;">{{$post->created_at->diffForHumans()}}</div>
-            <div class="detail">
+            <div class="detail py-2">
               <a href="{{route('post',$post->slug)}}"><h4 class="pb-20">{{$post->title}}</h4></a>
-              <p>
-              {!!Str::limit($post->body,400)!!}
-              </p>
-              <p class="" footer="">
+
+              <div>
+                <p>
+                    {{-- {!!Str::limit($post->body,300)!!} --}}
+                    {{-- {{ Str::limit($your_string, 50) }} --}}
+                    {{-- {!! Str::limit($post->body,20,'...') !!} --}}
+                    {!!  substr(strip_tags($post->body), 0, 200) !!}
+                    </p>
+              </div>
+              <p class="py-2" footer="">
                 <br>
-                </p><ul class="d-flex space-around">
-                    <li><a href="javascript:void(0);" onclick=" toastr.info('To add to your favorite list you have to login first.', 'Info', { closeButton: true, progressBar: true, })"><i class="fa fa-heart-o" aria-hidden="true"></i><span> {{$post->likedUsers->count()}} </span></a></li>
-                  <li><i class="fa fa-comment-o" aria-hidden="true"></i><span> {{$post->comments->count()}} </span></li>
-                  <li><i class="fa fa-eye" aria-hidden="true"></i> <span> {{$post->view_count}} </span></li>
-                </ul>
+                </p>
+
+          <div >
+            <ul   style="display: flex;
+            gap: 23px;margin-top: 95px;">
+            <li>
+                <a href="javascript:void(0);" onclick=" toastr.info('To add to your favorite list you have to login first.', 'Info', { closeButton: true, progressBar: true, })">
+                <i class="fa fa-heart-o" aria-hidden="true"></i>
+                <span> {{$post->likedUsers->count()}} </span>
+            </a>
+        </li>
+          <li><i class="fa fa-comment-o" aria-hidden="true"></i><span> {{$post->comments->count()}} </span></li>
+          <li><i class="fa fa-eye" aria-hidden="true"></i> <span> {{$post->view_count}} </span></li>
+        </ul>
+          </div>
+
 
 
             </div>
@@ -155,7 +172,7 @@
           <br>
           <p>I am <span class="c1">Full stack Web Developer</span> specialized <span class="c1">LARAVEL</span> - PHP. Currently Studing GEOSPATIAL SCIENCE and learning <span class="c1">GIS Web Applications Development</span>. </p>
           <br>
-          <h4>Email: <span style="font-size: medium; font-weight: lighter;">subhadipghorui105@gmail.com</span></h4>
+          <h4>Email: <span style="font-size: medium; font-weight: lighter;">mahmudaaktermunny4@gmail.com</span></h4>
           <br>
           <div class="col-md-12 d-flex justify-content-center py-3 mt-2">
             <a href="https://subhadipghorui.github.io" class="genric-btn info circle arrow mr-md-auto" target="_blank">Know More<span class="lnr lnr-arrow-right"></span></a>
@@ -165,14 +182,14 @@
           <div class="row">
             <div class="single-team">
               <div class="thumb">
-                <img class="img-fluid w-75 mx-auto" src="{{asset('frontend/img/admin.png')}}" alt="admin">
+                <img class="img-fluid w-75 mx-auto" src="{{asset('frontend/img/mahmuda-akter-munny6425f563bda1c.jpg')}}" alt="admin">
                 <div class="align-items-center justify-content-center d-flex">
                   <a href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
                   <a href="#"><i class="fab fa-youtube" aria-hidden="true"></i></a>
                 </div>
               </div>
               <div class="meta-text mt-30 text-center">
-                <h4>Subhadip Ghorui</h4>
+                <h4>Mahmuda Akter Munny</h4>
                 <p>Creator</p>
               </div>
             </div>
